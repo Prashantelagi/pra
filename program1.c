38a0f897b6a5b0d6fc5eddd7c84f5e501d3754e4
Prashantelagi <prashant.elagi123@gmail.com>
2024-11-04 15:01:27 +0530

    Update program1.c

--- program1.c
+++ program1.c
@@ -5,7 +5,7 @@ void main
 	print("hello world");
 	printf("Enter the World");
  i have added this line  in remotrepo on saturday
-this line i m adding in mylocal repo on monday
+this line i m adding in mylocal repotory on monday
  
 int main()         
 {
